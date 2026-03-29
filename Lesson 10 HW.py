5b0ded58d02f5327a639d165db688d6faa4c067b
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-03-29 13:47:17 +0100

    initial commit

---
+++ Lesson 10 HW.py
@@ -0,0 +1,5 @@
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the power(n): "))
+result = base ** exponent
+print (base, "raised to the power of ", exponent,"is equal to", result)
+print ("So, the final result is", result)
